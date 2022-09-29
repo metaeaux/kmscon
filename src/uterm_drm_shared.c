3004d0f62224951e0aaf660196aff320de625086
vanfanel <redwindwanderer@gmail.com>
2022-09-29 20:27:15 +0200

    Use current KMS videomode by default and add an option to control this.

--- src/uterm_drm_shared.c
+++ src/uterm_drm_shared.c
@@ -585,17 +585,6 @@ int uterm_drm_video_find_crtc(struct uterm_video *video, drmModeRes *res,
 	return -1;
 }
 
-static drmModeCrtc *get_current_crtc(int fd, uint32_t encoder_id)
-{
-	drmModeEncoder *enc = drmModeGetEncoder(fd, encoder_id);
-	if (enc) {
-		int crtc_id = enc->crtc_id;
-		drmModeFreeEncoder(enc);
-		return drmModeGetCrtc(fd, crtc_id);
-	}
-	return NULL;
-}
-
 static void bind_display(struct uterm_video *video, drmModeRes *res,
 			 drmModeConnector *conn)
 {
@@ -603,7 +592,6 @@ static void bind_display(struct uterm_video *video, drmModeRes *res,
 	struct uterm_display *disp;
 	struct uterm_drm_display *ddrm;
 	struct uterm_mode *mode;
-	drmModeCrtc *current_crtc;
 	int ret, i;
 
 	ret = display_new(&disp, vdrm->display_ops);
@@ -611,7 +599,9 @@ static void bind_display(struct uterm_video *video, drmModeRes *res,
 		return;
 	ddrm = disp->data;
 
-	current_crtc = get_current_crtc(vdrm->fd, conn->encoder_id);
+	drmModeEncoder *enc = drmModeGetEncoder(vdrm->fd, conn->encoder_id);
+	drmModeCrtc *current_crtc = drmModeGetCrtc(vdrm->fd, enc->crtc_id);
+	drmModeFreeEncoder(enc);
 
 	for (i = 0; i < conn->count_modes; ++i) {
 		ret = mode_new(&mode, &uterm_drm_mode_ops);
@@ -631,7 +621,7 @@ static void bind_display(struct uterm_video *video, drmModeRes *res,
 			disp->default_mode = mode;
 
 		/* Save the original KMS mode for later use */
-		if (current_crtc && memcmp(&conn->modes[i], &current_crtc->mode, sizeof(conn->modes[i])) == 0)
+		if (memcmp(&conn->modes[i], &current_crtc->mode, sizeof(conn->modes[i])) == 0)
 		 	disp->original_mode = mode;
 
 		uterm_mode_unref(mode);
